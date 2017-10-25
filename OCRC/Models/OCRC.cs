2ab492b16a1f4188fd3ff2bdcd07daf74dd4e65e
Nasser <nasserbinshabeeb@mail.weber.edu>
2017-10-25 12:37:10 -0600

    Created the database , (empty db). (will be ignored by the gitIgnore file.
    Added OCRC.cs to initialize the global.asax.cs and Web.config.

---
+++ OCRC/Models/OCRC.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel;
+
+namespace OCRC.Models
+{
+    public class OCRC
+    {
+    }
+
+
+    public class OCRCDbContext : DbContext
+    {
+
+    }
+}
