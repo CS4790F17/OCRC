cf8d0704e6cb805549966b9e2eae51fb86fd086e
Hoang Cao <hoangcao@mail.weber.edu>
2017-11-13 09:39:43 -0700

    Add Forgot Page and List PAge

---
+++ OCRC/Scripts/Login.js
@@ -0,0 +1,43 @@
+﻿$(document).ready(function () {
+    testLocalStorageData();
+    loadProfile();
+});
+
+
+function getLocalProfile(callback) {
+    var profileName = sessionStorage.getItem("PROFILE_NAME");
+    var profileReAuthEmail = sessionStorage.getItem("PROFILE_REAUTH_EMAIL");
+
+    if (profileName !== null
+        && profileReAuthEmail !== null) {
+        callback(profileName, profileReAuthEmail);
+    }
+    else {
+        location.href = 'Result.cshtml';
+    }
+}
+
+function loadProfile() {
+    if (!supportsHTML5Storage()) { return false; }
+    getLocalProfile(function (profileName, profileReAuthEmail) {
+        //changes in the UI
+        $("#profile-name").html(profileName);
+        $("#reauth-email").html(profileReAuthEmail);
+        $("#inputEmail").hide();
+        $("#remember").hide();
+    });
+}
+
+function supportsHTML5Storage() {
+    try {
+        return 'sessionStorage' in window && window['sessionStorage'] !== null;
+    } catch (e) {
+        return false;
+    }
+}
+
+function testLocalStorageData() {
+    if (!supportsHTML5Storage()) { return false; }
+    sessionStorage.setItem("PROFILE_NAME", "Hoang Cao");
+    sessionStorage.setItem("PROFILE_REAUTH_EMAIL", "hoangcao@mail.weber.edu");
+}
