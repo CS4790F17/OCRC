ccd84ea1264cb7009c7bb0417c8106e0eb3480cf
Tim3Ds@gmail.com <Tim3Ds@gmail.com>
2017-11-29 12:08:35 -0700

    add access stuff

--- OCRC/App_Start/BundleConfig.cs
+++ OCRC/App_Start/BundleConfig.cs
@@ -21,7 +21,8 @@ namespace OCRC
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
+                      "~/Scripts/respond.js",
+                      "~/Scripts/access.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/Login").Include(
                       "~/Scripts/Login.js"));
