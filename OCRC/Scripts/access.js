20c670722e5e73c9e8f944fa40127069a67cd67b
Tim3Ds@gmail.com <Tim3Ds@gmail.com>
2017-12-04 10:48:12 -0700

    update access

--- OCRC/Scripts/access.js
+++ OCRC/Scripts/access.js
@@ -4,6 +4,6 @@
         $('#userLinks').append('<li><a href="/Account/UserList">Admin</a></li>');
     }
     if ($('input[name=access]').val() == '3' || $('input[name=access]').val() == '4') {
-        $('#userLinks').append('<li><a href="/Account/UserList">Team Or School</a></li>');
+        $('#userLinks').append('<li><a href="/home/result">Team Or School</a></li>');
     }
 });
